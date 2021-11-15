6a6a749a344e78bc6c22d02f3c1895c7ad455eeb
vijay-desigan <66814488+vijay-desigan@users.noreply.github.com>
2021-11-15 23:18:56 +0530

    Add files via upload

---
+++ TIme-Conversion.py
@@ -0,0 +1,29 @@
+# Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+# Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+# - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+# Example
+# s='12:01:00PM'
+#   Return '12:01:00'.
+# s='12:01:00AM'
+#   Return '00:01:00'
+
+in_12='06:40:22AM'
+in_24='00:00:00'
+
+if in_12[8]=='P':
+    hr=int(in_12[0])*10+int(in_12[1])
+    if hr==12:
+        hr=12
+    else:
+        hr=hr+12
+    s2=str(hr)
+elif in_12[8]=='A':
+    hr=int(in_12[0])*10+int(in_12[1])
+    if hr==12:
+        hr='00'
+    s2=str(hr)
+for i in range(2,8):
+    s2=s2+in_12[i]
+print(s2)
+
+
