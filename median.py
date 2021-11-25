2810d55d0a1828f4a305bdeeeaa124b279e4c9ba
vijay-desigan <66814488+vijay-desigan@users.noreply.github.com>
2021-11-25 09:58:55 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,13 @@
+arr=[5,3,1,2,4]
+
+arr.sort()
+
+if len(arr)%2!=0:
+    ind=int((len(arr)-1)/2)
+    print(arr[ind])
+else:
+    ind1= len(arr)/2
+    ind2=len(arr)/2-1
+    median=(arr[ind1]+arr[ind2])/2
+    print(median)
+
