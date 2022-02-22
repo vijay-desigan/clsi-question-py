1317ef2e1cca2ffeb4cd909899edd0d39aa79d84
vijay-desigan <66814488+vijay-desigan@users.noreply.github.com>
2022-02-22 06:19:02 +0530

    Update ExcelSheetColumnNumber.py

--- ExcelSheetColumnNumber.py
+++ ExcelSheetColumnNumber.py
@@ -1,32 +1,32 @@
-171. Excel Sheet Column Number ---Leetcode
-Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
+# 171. Excel Sheet Column Number ---Leetcode
+# Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
 
-For example:
+# For example:
 
-A -> 1
-B -> 2
-C -> 3
-...
-Z -> 26
-AA -> 27
-AB -> 28 
-...
+# A -> 1
+# B -> 2
+# C -> 3
+# ...
+# Z -> 26
+# AA -> 27
+# AB -> 28 
+# ...
  
 
-Example 1:
+# Example 1:
 
-Input: columnTitle = "A"
-Output: 1
+# Input: columnTitle = "A"
+# Output: 1
   
-Example 2:
+# Example 2:
 
-Input: columnTitle = "AB"
-Output: 28
+# Input: columnTitle = "AB"
+# Output: 28
   
-Example 3:
+# Example 3:
 
-Input: columnTitle = "ZY"
-Output: 701
+# Input: columnTitle = "ZY"
+# Output: 701
 
   class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
