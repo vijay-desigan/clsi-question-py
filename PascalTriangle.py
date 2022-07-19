66b3d8ddee33ae9656cc40e5b8506c60b6e11dfd
vijay-desigan <66814488+vijay-desigan@users.noreply.github.com>
2022-07-19 06:17:09 +0530

    Create PascalTriangle.py

---
+++ PascalTriangle.py
@@ -0,0 +1,36 @@
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+# Example 1:
+
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+# Example 2:
+
+# Input: numRows = 1
+# Output: [[1]]
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans=[[1],[1,1]]
+        if numRows==1:
+            return [[1]]
+        elif numRows==2:
+            return [[1],[1,1]]
+        for i in range(1,numRows-1):
+            temp=[1]
+            t=1
+            print(ans[1])
+            while t<len(ans[i]):
+                s=ans[i][t-1]+ans[i][t]
+                temp.append(s)
+                t+=1
+                print(temp)
+            temp.append(1)
+            ans.append(temp)
+        return ans
